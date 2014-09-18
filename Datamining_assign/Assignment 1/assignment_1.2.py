4566a21ce106dcc4232b4d3cf06049f0def8f608
GSamuel <gideon_hoog@hotmail.com>
2014-09-18 10:34:48 +0200

    assignment 1.2 thingies

---
+++ Datamining_assign/Assignment 1/assignment_1.2.py
@@ -0,0 +1,29 @@
+"""
+Created on Thu Sep 18 09:26:20 2014
+
+@author: Gideon
+"""
+
+from xlrd import open_workbook
+from pylab import *
+import numpy as np
+
+file = 'Data/nanonose.xls'
+
+book = open_workbook(file)
+sheet = book.sheet_by_index(0) #select the correct sheet
+
+#print sheet.col_values(1,2)
+#print sheet.cell(0,0).value == 'Nanonose'
+
+
+print '\n\n'
+
+array = np.zeros(shape=(90,8))
+
+for row_index in range(2,sheet.nrows):
+    row = np.array(sheet.row_values(row_index,3))
+    array[row_index-2] = row
+
+print array[:,:2]
+
